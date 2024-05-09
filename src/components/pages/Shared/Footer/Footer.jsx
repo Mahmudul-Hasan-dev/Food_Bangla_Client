acd7ed752c9fc79fd68f3978e105f5f2d6e04d94
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2024-05-09 13:26:48 +0600

    footer

---
+++ src/components/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div >
+            <p className='my-0 py-2 text-center bg-dark text-white'>Food Bangla &copy; 2024 All rights reserved. </p>
+        </div>
+    );
+};
+
+export default Footer;
